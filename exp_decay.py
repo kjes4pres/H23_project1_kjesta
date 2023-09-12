78a5eb5cb6770cf405d4fffaa533f9c0660212fb
kjes4pres <stangelandkjersti@gmail.com>
2023-09-12 11:12:58 +0200

    Completed task 1a). Have not made code pretty yet.

--- exp_decay.py
+++ exp_decay.py
@@ -2,6 +2,7 @@ import scipy as sc
 import numpy as np
 
 class ExponentialDecay():
+
     def __init__(self, decay_constant) -> None:
         if decay_constant < 0:
             raise ValueError("The decay constant cannot be negative.")
@@ -10,4 +11,14 @@ class ExponentialDecay():
 
 
     def __call__(self, t: float, u: np.ndarray) -> np.ndarray:
-        ...
+        '''
+        Compute the derivative of u at time t.
+
+        Args:
+        t (float): time
+        u (array) : value of function
+
+        Returns:
+        array, derivative of u at time t.
+        '''
+        return -self.decay_constant*u
--- test_exp_decay.py
+++ test_exp_decay.py
@@ -4,6 +4,25 @@ import pytest
 
 from exp_decay import ExponentialDecay
 
-def test_constructor_raises_ValueError_for_negatives():
+def test_negative_decay_raises_ValueError_constructor():
     with pytest.raises(ValueError):
         instance = ExponentialDecay(-1)
+
+
+def test_rhs():
+    # Test example:
+    a = 0.4
+    u = np.array([3.2])
+    t = 0.0
+    model = ExponentialDecay(0.4)
+    computed = model(t, u)
+
+    # Variable expected is what the computed should be close to.
+    tol = 1e-6
+    expected = -1.28
+
+    diff = abs(computed - expected)
+    success = diff < tol
+    message = "Computed derivative differs from the expected value with {diff}."
+    assert success, message
+
