07bed12ee2bc82292a3a7891e41c1d5ee24a3241
kjes4pres <stangelandkjersti@gmail.com>
2023-09-19 10:40:06 +0200

    Test file for pendulum class. Made tests to check rhs of ODE and test to chek that pendulum at rest stays at rest.

---
+++ test_pendulum.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pytest
+
+from pendulum import *
+
+tol = 1e-2
+
+def test_rhs():
+    u = np.array([np.pi/6, 0.35])
+    du_dt_expected = np.array([0.35, -3.45])
+    model = Pendulum(M = 1, L = 1.42)
+    du_dt_computed = model(u)
+
+    for i, (comp, exp) in enumerate(zip(du_dt_computed, du_dt_expected)):
+        diff = abs(comp - exp)
+        success = diff < tol
+        msg = f"Computed derivative, {comp}, does not match the analytical, {exp}."
+        assert success, msg
+
+
+def test_pendulum_at_equilibrium_stays_at_rest():
+    u = np.array([0, 0])  
+    du_dt_expected = u
+    model = Pendulum(M = 1)
+    du_dt_computed = model(u)
+
+    for i, (comp, exp) in enumerate(zip(du_dt_computed, du_dt_expected)):
+        success = comp == exp
+        msg = f"Computed derivative, {comp}, does not match the analytical, {exp}."
+        assert success, msg
+
+
+
+    
